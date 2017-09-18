c8170ad72845fa86e6467ab7f0fce71814ecdb5e
Suyash Kumar <suyash.kumar@duke.edu>
2017-09-18 10:43:48 -0400

    Initial travis ci anaconda test

---
+++ add.py
@@ -0,0 +1,6 @@
+def add(a, b): 
+    s = a + b
+    if s < 0:
+        return 0
+    return s
+   
---
+++ test_add.py
@@ -0,0 +1,5 @@
+from add import add
+
+def test_add_positive():
+    r = add(1, 3)
+    assert r == 4
